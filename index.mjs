4727a456809cd4410d02482ccd18c5e6b635936a
artachojf <pepe.artacho.martin@uma.es>
2023-11-08 12:41:37 +0100

    API v1

--- index.mjs
+++ index.mjs
@@ -1,6 +1,5 @@
-import { ObjectId } from "mongodb";
-import clientes from "./conn.mjs";
 import express from "express";
+import v1 from "./v1.mjs";
 
 const app = express();
 const port = 5000;
@@ -12,88 +11,4 @@ app.listen(port, () => {
   console.log(`Now listening on port ${port}`);
 });
 
-app.get("/", async (req, res) => {
-  try {
-    let filtro = {};
-    let orden = {};
-
-    const queries = req.query;
-
-    if (queries.name) {
-      filtro = { ...filtro, name: queries.name };
-    }
-    if (queries.email) {
-      filtro = { ...filtro, email: queries.email };
-    }
-    if (queries.googleId) {
-      filtro = { ...filtro, googleId: queries.googleId };
-    }
-    if (queries.oauthToken) {
-      filtro = { ...filtro, oauthToken: queries.oauthToken };
-    }
-
-    if(queries.orderBy && queries.order) {
-      if (queries.order == "asc") {
-          orden = { ...orden, [queries.orderBy]: 1 };
-      } else if (queries.order == "desc") {
-          orden = { ...orden, [queries.orderBy]: -1 };
-      }
-  }
-
-    let results = await clientes.find(filtro).sort(orden).toArray();
-    res.send(results).status(200);
-  } catch (e) {
-    res.send(e).status(500);
-  }
-});
-
-app.post("/", async (req, res) => {
-  try {
-    const cliente = req.body;
-    const result = await clientes.insertOne(cliente);
-    res.send(result).status(200);
-  } catch (e) {
-    res.send(e).status(500);
-  }
-});
-
-app.get("/:id", async (req, res) => {
-  try {
-    const result = await clientes.findOne({ _id: new ObjectId(req.params.id) });
-    res.send(result).status(200);
-  } catch (e) {
-    res.send(e).status(500);
-  }
-});
-
-app.delete("/:id", async (req, res) => {
-  try {
-    const result = await clientes.deleteOne({
-      _id: new ObjectId(req.params.id),
-    });
-    res.send(result).status(200);
-  } catch (e) {
-    res.send(e).status(500);
-  }
-});
-
-app.delete("/", async (req, res) => {
-  try {
-    let result = await clientes.deleteMany(req.body);
-    res.send(result).status(200);
-  } catch (e) {
-    res.send(e).status(500);
-  }
-});
-
-app.put("/:id", async (req, res) => {
-  try {
-    const result = await clientes.updateOne(
-      { _id: new ObjectId(req.params.id) },
-      { $set: req.body }
-    );
-    res.send(result).status(200);
-  } catch (e) {
-    res.send(e).status(500);
-  }
-});
+app.use("/v1", v1);
---
+++ v1.mjs
@@ -0,0 +1,93 @@
+import { ObjectId } from "mongodb";
+import clientes from "./conn.mjs";
+import express from "express";
+
+const app = express.Router();
+
+app.get("/", async (req, res) => {
+    try {
+        let filtro = {};
+        let orden = {};
+
+        const queries = req.query;
+
+        if (queries.name) {
+            filtro = { ...filtro, name: queries.name };
+        }
+        if (queries.email) {
+            filtro = { ...filtro, email: queries.email };
+        }
+        if (queries.googleId) {
+            filtro = { ...filtro, googleId: queries.googleId };
+        }
+        if (queries.oauthToken) {
+            filtro = { ...filtro, oauthToken: queries.oauthToken };
+        }
+
+        if (queries.orderBy && queries.order) {
+            if (queries.order == "asc") {
+                orden = { ...orden, [queries.orderBy]: 1 };
+            } else if (queries.order == "desc") {
+                orden = { ...orden, [queries.orderBy]: -1 };
+            }
+        }
+
+        let results = await clientes.find(filtro).sort(orden).toArray();
+        res.send(results).status(200);
+    } catch (e) {
+        res.send(e).status(500);
+    }
+});
+
+app.post("/", async (req, res) => {
+    try {
+        const cliente = req.body;
+        const result = await clientes.insertOne(cliente);
+        res.send(result).status(200);
+    } catch (e) {
+        res.send(e).status(500);
+    }
+});
+
+app.get("/:id", async (req, res) => {
+    try {
+        const result = await clientes.findOne({ _id: new ObjectId(req.params.id) });
+        res.send(result).status(200);
+    } catch (e) {
+        res.send(e).status(500);
+    }
+});
+
+app.delete("/:id", async (req, res) => {
+    try {
+        const result = await clientes.deleteOne({
+            _id: new ObjectId(req.params.id),
+        });
+        res.send(result).status(200);
+    } catch (e) {
+        res.send(e).status(500);
+    }
+});
+
+app.delete("/", async (req, res) => {
+    try {
+        let result = await clientes.deleteMany(req.body);
+        res.send(result).status(200);
+    } catch (e) {
+        res.send(e).status(500);
+    }
+});
+
+app.put("/:id", async (req, res) => {
+    try {
+        const result = await clientes.updateOne(
+            { _id: new ObjectId(req.params.id) },
+            { $set: req.body }
+        );
+        res.send(result).status(200);
+    } catch (e) {
+        res.send(e).status(500);
+    }
+});
+
+export default app;
