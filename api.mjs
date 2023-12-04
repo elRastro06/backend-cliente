1babdd54bf25a3fcc912d4e5d23c94890c33db1c
FerJRL <frl711m@uma.es>
2023-12-04 17:57:32 +0100

    bug menor solucionado

--- api.mjs
+++ api.mjs
@@ -3,6 +3,6 @@ const urlReviews = "http://localhost:5008/v2"
 
 
 export const getReviewsByReviewedID = async (id) => {
-  const response = await axios.get(`${urlReviews}/?eviewedID=${id}`);
+  const response = await axios.get(`${urlReviews}/?reviewedID=${id}`);
   return response.data;
 };
