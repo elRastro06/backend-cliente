d28ee1674d7e4bc3f264d67e05854245ba3a1133
FerJRL <frl711m@uma.es>
2023-12-09 22:07:23 +0100

    reviews average

--- v2.mjs
+++ v2.mjs
@@ -7,8 +7,25 @@ const app = express.Router();
 
 app.get("/:id/reviews", async (req, res) => {
   try {
-    const pujas = await getReviewsByReviewedID(req.params.id);
-    res.send(pujas).status(200);
+    const reviews = await getReviewsByReviewedID(req.params.id);
+    res.send(reviews).status(200);
+  } catch (e) {
+    res.send(e).status(500);
+  }
+});
+
+app.get("/:id/reviewsavg", async (req, res) => {
+  try {
+    const reviews = await getReviewsByReviewedID(req.params.id);
+
+    let reviewAvg = 0;
+    reviews.forEach(p => {
+      reviewAvg += p.rating;
+    });
+    
+    reviewAvg = reviewAvg / reviews.length;
+
+    res.send( {reviewAvg: reviewAvg } ).status(200);
   } catch (e) {
     res.send(e).status(500);
   }
